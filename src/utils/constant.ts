6765a8320da7dfe0e2ba96740366267db9ec548c
mac <mac@192.168.1.121>
2023-03-22 10:41:02 +0800

    initial

--- src/utils/constant.ts
+++ src/utils/constant.ts
@@ -1,7 +1,7 @@
 const agoraParams = {
-    appId: 'f3098418ac0a4610ba69339534a228aa',
-    token: '007eJxTYPjzakLakeIS9hblFxwf3v+d08Fpd6G9h/vJzQ8su53OM+5UYEgzNrC0MDG0SEw2SDQxMzRISjSzNDa2NDU2STQyskhM3GDPk9IQyMjg+luViZEBAkF8NobijMzCTBMGBgAH/CC7',
-    channelName: 'shiqi4',
+    appId: '',
+    token: '',
+    channelName: '',
     uid: 0, // 本地用户，无需更改
 }
 
